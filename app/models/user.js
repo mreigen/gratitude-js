6f2addcc218440432b0d3fcbc225315601293930
minh <mreigen@gmail.com>
2014-03-05 23:14:12 -0800

    mongoose model user to have extra info from Facebook

--- app/models/user.js
+++ app/models/user.js
@@ -1,6 +1,10 @@
 var mongoose = require('mongoose');
 
 module.exports = mongoose.model('User', {
+  name: String,
   username : String,
-  email : String
+  email : String,
+  gender : String,
+  photoUrl : String,
+  data : Object
 });
